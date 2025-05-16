585893b1614ba2982521dee2c3f2eb4db47a9115
kosachin <kosachin313@gmail.com>
2025-05-16 23:30:22 +0530

    minor fix

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UsersModule } from './users/users.module';
-import { DatabaseModule } from '@app/common';
 
 @Module({
-  imports: [UsersModule, DatabaseModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
